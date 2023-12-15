22c3fa214b3c11306eb614f04e04d5d7abc2681a
Aryan Uknai <63489402+aryan2434@users.noreply.github.com>
2023-12-15 14:27:37 -0500

    question 3a complete

--- src/main/java/com/example/comp1011st200544014test2/CustomerViewController.java
+++ src/main/java/com/example/comp1011st200544014test2/CustomerViewController.java
@@ -1,12 +1,17 @@
 package com.example.comp1011st200544014test2;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
+
+import java.util.List;
 
 public class CustomerViewController {
 
@@ -53,13 +58,46 @@ public class CustomerViewController {
     private TableView<Customers> tableView;
 
     @FXML
-    void getAllCustomer(ActionEvent event) {
+    public void initialize() {
+        List<Business> businessData = Utility.getBusinessData("customers.json");
 
+        if (!businessData.isEmpty()) {
+            Business business = businessData.get(0);
+
+            ObservableList<Customers> customerList = FXCollections.observableArrayList(business.getCustomers());
+
+
+            id.setCellValueFactory(new PropertyValueFactory<>("id"));
+            firstName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+            lastName.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+            city.setCellValueFactory(new PropertyValueFactory<>("city"));
+
+            // Set up cell factory for purchaseColumn to format currency values
+            purchase.setCellValueFactory(data -> new SimpleObjectProperty<>(data.getValue().getPurchaseValue()));
+            tableView.setItems(customerList);
+            numOfCustomer.setText("Number of Customers: "+ tableView.getItems().size());
+
+            purchase.setCellFactory(tc -> new TableCell<Customers, Double>() {
+                @Override
+                protected void updateItem(Double value, boolean empty) {
+                    super.updateItem(value, empty);
+                    if (empty || value == null) {
+                        setText(null);
+                    } else {
+                        setText(String.format("$%.2f", value));
+                    }
+                }
+            });
+
+            tableView.refresh();
+        }}
+    @FXML
+    void getAllCustomer(ActionEvent event) {
+        initialize();
     }
 
     @FXML
     void getHigherSavingCustomer(ActionEvent event) {
 
     }
-
 }
