108b429fc5aa58fbaf4fc04732571b421d963ff3
Aryan Uknai <63489402+aryan2434@users.noreply.github.com>
2023-12-15 12:08:41 -0500

    dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module com.example.comp1011st200544014test2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+    requires com.google.gson;
             
                             
-    opens com.example.comp1011st200544014test2 to javafx.fxml;
+    opens com.example.comp1011st200544014test2 to javafx.fxml, com.google.gson;
     exports com.example.comp1011st200544014test2;
 }
